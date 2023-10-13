7fd1eab74e8b19c9ae62f216a42eca1f043b935d
mengtee <mengtee1127@gmail.com>
2023-10-14 02:33:27 +0800

    adding pages and components file

---
+++ components/Dataset.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const Dataset = () => {
+  return (
+    <div>
+      <h1>Dataset Page</h1>
+      <p>This is the dataset page content.</p>
+    </div>
+  );
+};
+
+export default Dataset;
---
+++ pages/dataset.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const DatasetPage = () => {
+  return (
+    <div>
+      <dataset />
+    </div>
+  );
+};
+
+export default DatasetPage;
