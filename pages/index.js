a9cc3c173fb62212967c87215f83652b3d514471
mengtee <mengtee1127@gmail.com>
2023-10-15 03:15:22 +0800

    update index

--- pages/index.js
+++ pages/index.js
@@ -10,18 +10,17 @@ import RootLayout from '@/components/RootLayout'
 const features = [
   {
     name: 'Tokenized Payments.',
-    description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+    description:"Conduct transactions with speed and security through our tokenized payment system.",
     icon: CloudArrowUpIcon,
   },
   {
-    name: 'Privacy and Security',
-    description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
+    name: 'Safety and Confidentiality.',
+    description: "We prioritize your safety and confidentiality with top-tier security features.",
     icon: LockClosedIcon,
   },
   {
-    name: 'Data Access Control.',
-    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+    name: 'Control Over Data Access.',
+    description: "Choose who gets access to what data, thanks to our specialized access protocols.",
     icon: ServerIcon,
   },
 ]
@@ -38,8 +37,7 @@ export default function Home() {
               <h2 className="text-base font-semibold leading-7 text-indigo-600">Discover, Access, and Trade Tokenized Data</h2>
               <p className="mt-2 text-3xl font-bold leading-9 tracking-tight text-gray-900 sm:text-4xl">Your Marketplace for Tokenized Data Sets</p>
               <p className="mt-6 text-lg leading-8 text-gray-600">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
-                iste dolor cupiditate blanditiis ratione.
+              In a world of complexity, our marketplace simplifies the way you interact with data. Experience unparalleled convenience and efficiency with our platform.
               </p>
               <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
                 {features.map((feature) => (
